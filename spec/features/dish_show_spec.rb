727f2e9282a46c6f15267d483fd176e9e711f1ef
Taylor Keller <takeller17@gmail.com>
2020-05-29 09:28:19 -0600

    Write spec for user-story-1

---
+++ spec/features/dish_show_spec.rb
@@ -0,0 +1,35 @@
+require "rails_helper"
+
+describe "As a visitor" do
+  describe "When I visit a dishes show page" do
+    it "I see a list of ingredients for this dish and the Chef that created it" do
+
+      chef = Chef.create(name: "Ben")
+      dish = Dish.create( name: "BBQ Pork Mac & Cheese",
+                          description: "Cheesy goodness",
+                          chef_id: chef.id)
+
+      cheese = Ingredient.create( name: "Cheese sauce",
+                                  calories: 500)
+      cavatappi = Ingredient.create(  name: "Cavatappi noodles",
+                                      calories: 500)
+      pork = Ingredient.create( name: "BBQ pork",
+                                calories: 500)
+
+      dish_ingredient1 = DishIngredient.create( dish_id: dish.id,
+                                                ingredient_id: cheese.id)
+      dish_ingredient2 = DishIngredient.create( dish_id: dish.id,
+                                                ingredient_id: cavatappi.id)
+      dish_ingredient2 = DishIngredient.create( dish_id: dish.id,
+                                                ingredient_id: pork.id)
+
+
+      visit "/dishes/#{dish.id}"
+
+      expect(page).to have_content(cheese.name)
+      expect(page).to have_content(cavatappi.name)
+      expect(page).to have_content(pork.name)
+      expect(page).to have_content(chef.name)
+    end
+  end
+end
