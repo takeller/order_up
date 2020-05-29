969814490bf4048451b5877dc3800c87f7837067
Taylor Keller <takeller17@gmail.com>
2020-05-29 09:37:13 -0600

    Write user-story-2 feature spec

--- spec/features/dish_show_spec.rb
+++ spec/features/dish_show_spec.rb
@@ -32,5 +32,30 @@ describe "As a visitor" do
       expect(page).to have_content(chef.name)
       save_and_open_page
     end
+
+    it "I see the total calorie count of the dish" do
+      chef = Chef.create(name: "Ben")
+      dish = Dish.create( name: "BBQ Pork Mac & Cheese",
+                          description: "Cheesy goodness",
+                          chef_id: chef.id)
+
+      cheese = Ingredient.create( name: "Cheese sauce",
+                                  calories: 500)
+      cavatappi = Ingredient.create(  name: "Cavatappi noodles",
+                                      calories: 500)
+      pork = Ingredient.create( name: "BBQ pork",
+                                calories: 500)
+
+      dish_ingredient1 = DishIngredient.create( dish_id: dish.id,
+                                                ingredient_id: cheese.id)
+      dish_ingredient2 = DishIngredient.create( dish_id: dish.id,
+                                                ingredient_id: cavatappi.id)
+      dish_ingredient2 = DishIngredient.create( dish_id: dish.id,
+                                                ingredient_id: pork.id)
+
+      visit "/dishes/#{dish.id}"
+
+      expect(page).to have_content("Total Calories: 1500")
+    end
   end
 end
