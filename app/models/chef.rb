b505bea9f52d6bc70b5fb19f148168b46d51233a
Taylor Keller <takeller17@gmail.com>
2020-05-29 10:09:50 -0600

    Write all_ingredients chef method

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,4 +2,13 @@ class Chef <ApplicationRecord
   validates_presence_of :name
   has_many :dishes
 
+  def all_ingredients
+    ingredients = []
+    dishes.each do |dish|
+      ingredients << dish.ingredients
+    end
+    ingredients.flatten!
+    ingredients.uniq!
+    ingredients
+  end
 end
